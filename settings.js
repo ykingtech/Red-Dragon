9455e598fbaaf06eea6f864f8171d59905133f74
ykingtech <92142721+ykingtech@users.noreply.github.com>
2022-07-21 05:22:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//               😂සුපිරි යකා😂
 //
 //════════════(00)════════════════\\
 
@@ -19,29 +19,29 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `බ්‍රෝ මම සිපිරි යකා🔥
+මාව හැදුවෙ මහඋලුගෙදර සුපිරි ජයන්තයා 😂` //Costomize Alive Message (type your message in `` )
 
 global.owner = ['+380944814219'] //Owner number in aive msg
 global.premium = ['+380944814219'] //Owner Number info
 global.ownernomer = '+380944814219' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎මහඋලුගෙදර සුපිරි ජයන්තයා' //Owner Name
+global.botname = ' ☬සුපිරි༒යකා࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '🙌යූ ටියුබ්🃏' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/c/YKINGTECHSHOW_y_king' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'අමුතු ඇටේ //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬සුපිරි༒යකා࿐' //Bot Pacage Name
+global.author = '😎මහඋලුගෙදර සුපිරි ජයන්තයා❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
-global.chatbotpre = 'true'  // Chat bot true/false
+global.footer = '© මහඋලුගෙදර සුපිරි ජයන්තයා' //In list menu
+global.chatbotpre = 'false'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
 
@@ -84,3 +84,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
